00e0d933311ec626c391a40557fea07a9ec28e2a
Akbal123 <akbaltonik123@gmail.com>
2016-12-02 15:02:50 +0700

    Add files via upload

---
+++ Komputer_Terapan.ino
@@ -0,0 +1,20 @@
+int analogInPin = A2;  
+int led1 = 5;
+int sensorValue = 0;      
+
+void setup() {
+Serial.begin(9600); 
+pinMode(led1, OUTPUT);
+}
+
+void loop() {
+sensorValue = analogRead(analogInPin);       
+if (sensorValue >=125)
+  {
+    digitalWrite(led1, HIGH);
+  }
+  else (sensorValue <=36)
+;  {
+    digitalWrite(led1, LOW);
+  }
+ }
